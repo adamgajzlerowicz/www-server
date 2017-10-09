33241ef684df1fb0317fb211cfe547b88e828b1b
adam.gajzlerowicz <nelf86@gmail.com>
2017-10-09 22:45:54 +0200

    push

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ dupa.use(serveStatic('dupa'));
 app.use(vhost('dupa.local', dupa));
 
 const czyjesttarta = connect();
-dupa.use(serveStatic('czyjesttarta.pl/public'));
+czyjesttarta.use(serveStatic('czyjesttarta.pl/public'));
 app.use(vhost('czyjesttarta.pl', czyjesttarta));
 
 const oktorejjestobiad = connect();
-dupa.use(serveStatic('oktorejjestobiad.pl'));
+oktorejjestobiad.use(serveStatic('oktorejjestobiad.pl'));
 app.use(vhost('oktorejjestobiad.pl', oktorejjestobiad));
 
 const indexApp = connect();
-dupa.use(serveStatic('app'));
+indexApp.use(serveStatic('app'));
 app.use(vhost('145.239.91.188', indexApp));
 
 app.listen(80);
